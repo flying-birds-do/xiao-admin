702b66d735039add7030e7a611c33913b488b5e4
xiaoyu.bi <xiaoyu.bi@lepudigital.com>
2023-03-20 00:19:00 +0800

    fix: ceshi

--- vite.config.ts
+++ vite.config.ts
@@ -7,37 +7,39 @@ import { resolve } from 'path'
 import Icons from 'unplugin-icons/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	resolve: {
-		alias: {
-			/** @ 符号指向 src 目录 */
-			'@': resolve(__dirname, './src'),
+export default ({ mode }) =>
+	defineConfig({
+		resolve: {
+			alias: {
+				/** @ 符号指向 src 目录 */
+				'@': resolve(__dirname, './src'),
+			},
 		},
-	},
-	plugins: [
-		vue(),
-		AutoImport({
-			resolvers: [ElementPlusResolver()],
-		}),
-		Components({
-			resolvers: [ElementPlusResolver()],
-		}),
-		Icons({
-			/* options */
-		}),
-	],
-	extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
-	server: {
-		open: true, // 自动打开浏览器
-		proxy: {
-			'/api': {
-				target: 'http://',
-				changeOrigin: true,
-				rewrite(path) {
-					return path.replace(/^\/api/, '')
+		plugins: [
+			vue(),
+			AutoImport({
+				resolvers: [ElementPlusResolver()],
+			}),
+			Components({
+				resolvers: [ElementPlusResolver()],
+			}),
+			Icons({
+				/* options */
+			}),
+		],
+		extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+		server: {
+			open: true, // 自动打开浏览器
+			proxy: {
+				'/api': {
+					target: 'http://',
+					changeOrigin: true,
+					rewrite(path) {
+						return path.replace(/^\/api/, '')
+					},
 				},
 			},
 		},
-	},
-	publicPath: process.env.BASE_URL,
-})
+		base: mode === 'production' ? '/xiao-admin/' : '/',
+		publicPath: process.env.BASE_URL,
+	})
