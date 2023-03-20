2a9fe52dacfd4a1b0855212db1ed0d6d84eaabd5
flying-birds-do <bixiaoyu825@163.com>
2023-03-20 08:49:44 +0000

    Deploying to gh-pages from @ flying-birds-do/xiao-admin@3ab9876092af5d3b7c558c7934faf23b99097f55 🚀

---
+++ assets/index-1966316d.js
@@ -0,0 +1,1 @@
+import{d as s,r as c,o as t,b as _,A as o,_ as n}from"./index-5a31fa09.js";const a={class:"vue-full-warp"},r=["src"],l=s({__name:"index",setup(d){const e=c("https://v3.cn.vuejs.org/");return(p,u)=>(t(),_("section",a,[o("iframe",{class:"wh-full",src:e.value},null,8,r)]))}});const f=n(l,[["__scopeId","data-v-13e4eceb"]]);export{f as default};
