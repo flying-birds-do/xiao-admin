f8dc168df9d1e8253290f25f90ba61212e3319cc
xiaoyu.bi <xiaoyu.bi@lepudigital.com>
2023-03-22 10:11:15 +0800

    fix: 注释包

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { resolve } from 'path'
 import Icons from 'unplugin-icons/vite'
 import viteCompression from 'vite-plugin-compression'
-import { Plugin as importToCDN } from 'vite-plugin-cdn-import'
+// import { Plugin as importToCDN } from 'vite-plugin-cdn-import'
 // import viteImagemin from 'vite-plugin-imagemin'
 import { visualizer } from 'rollup-plugin-visualizer'
 // https://vitejs.dev/config/
@@ -31,18 +31,18 @@ export default ({ mode }) =>
 			}),
 			// gzip压缩
 			viteCompression(),
-			importToCDN({
-				modules: [
-					{
-						// 需要 CDN 加速的包名称
-						name: 'lodash',
-						// 全局分配给模块的变量，Rollup 需要这个变量名称
-						var: 'lodash',
-						// 指定 CDN 上的加载路径
-						path: `https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js`,
-					},
-				],
-			}),
+			// importToCDN({
+			// 	modules: [
+			// 		{
+			// 			// 需要 CDN 加速的包名称
+			// 			name: 'lodash',
+			// 			// 全局分配给模块的变量，Rollup 需要这个变量名称
+			// 			var: 'lodash',
+			// 			// 指定 CDN 上的加载路径
+			// 			path: `https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js`,
+			// 		},
+			// 	],
+			// }),
 			// 图片压缩
 			// viteImagemin({
 			// 	gifsicle: {
