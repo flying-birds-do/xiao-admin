7ff672f862076ac41a0a0b58b67c01efc5d53099
flying-birds-do <bixiaoyu825@163.com>
2023-03-28 16:30:41 +0000

    Deploying to gh-pages from @ flying-birds-do/xiao-admin@68dac7c83aa0d75ba3197c2472bda1f9a3206976 🚀

---
+++ assets/index-6be2773b.js
@@ -0,0 +1,7 @@
+import{J as r,a as e,r as c,o as l,q as p,W as a,F as v,t as i}from"./vue-27b7826a.js";const _=i("div",{class:"a"},"v-md-editor预览组件测试：",-1),f=r({__name:"index",setup(m){const n=e(`# 序
+        开始预览组件的使用
+* 看
+* 写
+* 运行`),o=e("## 1. 我的威廉斯"),s=e(`let和const是JavaScript里相对较新的变量声明方式。 像我们之前提到过的， let在很多方面与var是相似的，但是可以帮助大家避免在JavaScript里常见一些问题。 const是对let的一个增强，它能阻止对一个变量再次赋值。
+因为TypeScript是JavaScript的超集，所以它本身就支持let和const。 下面我们会详细说明这些新的声明方式以及为什么推荐使用它们来代替 var。
+如果你之前使用JavaScript时没有特别在意，那么这节内容会唤起你的回忆。 如果你已经对 var声明的怪异之处了如指掌，那么你可以轻松地略过这节。`);return(d,u)=>{const t=c("v-md-preview");return l(),p(v,null,[_,a(t,{text:n.value},null,8,["text"]),a(t,{text:o.value},null,8,["text"]),a(t,{text:s.value},null,8,["text"])],64)}}});export{f as default};
